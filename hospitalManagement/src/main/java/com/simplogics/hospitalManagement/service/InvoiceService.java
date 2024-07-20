cbc68a6d8ee6774c076d2f87cb6b3357d09bee9f
sreedev <xreedev@gmail.com>
2024-07-20 23:46:22 +0530

    interfaces extracted

--- hospitalManagement/src/main/java/com/simplogics/hospitalManagement/service/InvoiceService.java
+++ hospitalManagement/src/main/java/com/simplogics/hospitalManagement/service/InvoiceService.java
@@ -1,35 +1,13 @@
 package com.simplogics.hospitalManagement.service;
-import com.simplogics.hospitalManagement.Invoice.Invoice;
+
+import com.simplogics.hospitalManagement.advice.InvalidDataFormatException;
+import com.simplogics.hospitalManagement.advice.NoDependencyException;
 import com.simplogics.hospitalManagement.advice.NullRequestException;
-import com.simplogics.hospitalManagement.model.Patient;
-import com.simplogics.hospitalManagement.repository.IPatientProcedureRepository;
-import com.simplogics.hospitalManagement.repository.IPatientRepository;
-import com.simplogics.hospitalManagement.util.ExcelUtil;
-import com.simplogics.hospitalManagement.util.InvoiceUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class InvoiceService {
-    private final IPatientRepository patientRepository;
-    private final IPatientProcedureRepository patientProcedureRepository;
-    public void createInvoice(Integer patientId,String dest) throws Exception {
-        Invoice invoice=InvoiceUtil.setInvoice(patientId,patientProcedureRepository);
-         InvoiceUtil.generateInvoice(invoice,dest);
-    }
-    public void createExcel(String dest) throws IOException, NullRequestException {
-        List<Invoice> invoices=new ArrayList<>();
-        List<Patient> patientIds=patientRepository.findAll();
-        for(Patient patient:patientIds) {
-            System.out.println(patient.getPatientId());
-            Invoice invoice=InvoiceUtil.setInvoice(patient.getPatientId(),patientProcedureRepository);
-            invoices.add(invoice);
-        }
-        ExcelUtil.generateInvoices(invoices,dest);
-    }
+public interface InvoiceService {
+    void createInvoice(Integer patientId, String destination) throws Exception;
 
+    void createExcel(String dest, String startDate, String endDate) throws IOException, NullRequestException, InvalidDataFormatException, NoDependencyException;
 }
