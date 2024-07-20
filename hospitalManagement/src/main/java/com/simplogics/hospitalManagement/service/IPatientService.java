bcde5771defb4187e4ad3891f96e7b68b87e6a48
sreedev <xreedev@gmail.com>
2024-07-20 23:46:59 +0530

    interfaces extracted

---
+++ hospitalManagement/src/main/java/com/simplogics/hospitalManagement/service/IPatientService.java
@@ -0,0 +1,9 @@
+package com.simplogics.hospitalManagement.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class IPatientService extends PatientService {
+}
